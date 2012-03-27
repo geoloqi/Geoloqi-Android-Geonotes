b01fd9f39e739de8418ae4bd6ffb411a8a5bc4b5
Tristan Waddington <tristan.waddington@gmail.com>
2012-03-27 14:32:53 -0700

    Reference the new ActionBarSherlock library as a library project. Update the default application themes.xml resource files. Implement the support ActionBar in the main activity.

--- src/com/geoloqi/android/ui/MainActivity.java
+++ src/com/geoloqi/android/ui/MainActivity.java
@@ -1,27 +1,38 @@
 package com.geoloqi.android.ui;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 import com.geoloqi.android.R;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentTransaction;
 
 /** Stub */
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockActivity implements TabListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        for (int i = 1; i <= 3; i++) {
+            ActionBar.Tab tab = getSupportActionBar().newTab();
+            tab.setText("Tab " + i);
+            tab.setTabListener(this);
+            getSupportActionBar().addTab(tab);
+        }
     }
-    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
+        MenuInflater inflater = getSupportMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
@@ -34,5 +45,23 @@ public class MainActivity extends Activity {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
+        
     }
 }
