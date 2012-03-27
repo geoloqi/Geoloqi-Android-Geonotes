a86843d63eab59daaee0ef39572cc0e47f80a287
Tristan Waddington <tristan.waddington@gmail.com>
2012-03-26 17:45:18 -0700

    Update the application to start the tracker when the app launches. Start the tracking service in foreground mode and display an ongoing notification [#13 state:resolved].

--- src/com/geoloqi/android/ui/LauncherActivity.java
+++ src/com/geoloqi/android/ui/LauncherActivity.java
@@ -1,11 +1,15 @@
 package com.geoloqi.android.ui;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.geoloqi.android.Constants;
 import com.geoloqi.android.R;
+import com.geoloqi.android.sdk.service.LQService;
 
 /**
  * This Activity is responsible for launching the Geoloqi client
@@ -25,6 +29,9 @@ public class LauncherActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
         
+        // Start up the tracking service
+        startTracker();
+        
         // Show the splash screen then start the main activity
         mHandler.postDelayed(new Runnable() {
             @Override
@@ -32,5 +39,39 @@ public class LauncherActivity extends Activity {
                 startActivity(new Intent(LauncherActivity.this, MainActivity.class));
             }
         }, SPLASH_TIMEOUT);
+    }
+    
+    /** Stub */
+    private void startTracker() {
+        Intent intent = new Intent(this, LQService.class);
+        intent.setAction(LQService.ACTION_FOREGROUND);
+        intent.putExtra(LQService.EXTRA_SDK_ID, Constants.GEOLOQI_ID);
+        intent.putExtra(LQService.EXTRA_SDK_SECRET, Constants.GEOLOQI_SECRET);
+        intent.putExtra(LQService.EXTRA_C2DM_SENDER, "geoloqi@gmail.com");
+        intent.putExtra(LQService.EXTRA_NOTIFICATION, getNotification());
+        startService(intent);
+    }
+    
+    /** Stub */
+    private PendingIntent getPendingIntent() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.setAction(Intent.ACTION_DEFAULT);
+        return PendingIntent.getActivity(this, 0, intent, 0);
+    }
+    
+    /** Stub */
+    private Notification getNotification() {
+        // TODO: Upgrade to use a Notification.Builder when more devices
+        //       are on API level 11+.
+        Notification notification = new Notification();
+        notification.flags |= Notification.FLAG_ONLY_ALERT_ONCE;
+        notification.icon = R.drawable.ic_stat_notify;
+        notification.tickerText = "The tracker is running.";
+        notification.when = System.currentTimeMillis();
+        notification.setLatestEventInfo(this, "Geoloqi", "The tracker is running.",
+                        getPendingIntent());
+        return notification;
     }
 }
