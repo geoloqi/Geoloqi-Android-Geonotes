94c8fc045a1bd9d5abc15d018627e0ff3f2078ee
Tristan Waddington <tristan.waddington@gmail.com>
2012-05-23 16:13:37 -0700

    Remove sample constants file.

--- src/com/geoloqi/android/ConstantsTemplate.java
+++
@@ -1,8 +0,0 @@
-package com.geoloqi.android;
-
-/** Stub */
-public class ConstantsTemplate {
-    public static final String GEOLOQI_ID = "";
-    public static final String GEOLOQI_SECRET = "";
-    public static final String GOOGLE_MAPS_API_KEY = "";
-}
