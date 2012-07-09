0432fc0cd6496a38008b636e8718114fcd406835
Tristan Waddington <tristan.waddington@gmail.com>
2012-07-09 15:27:00 -0700

    Don't launch the main activity if the splash screen was dismissed.

--- src/com/geoloqi/geonotes/ui/LauncherActivity.java
+++ src/com/geoloqi/geonotes/ui/LauncherActivity.java
@@ -18,6 +18,14 @@ public class LauncherActivity extends Activity {
     /** The minimum amount of time that the splash screen will be displayed. */
     private static final int SPLASH_TIMEOUT = 2000;
     
+    /** A runnable that will launch the main activity. */
+    private Runnable mStartMainActivity = new Runnable() {
+        @Override
+        public void run() {
+            startActivity(new Intent(LauncherActivity.this, MainActivity.class));
+        }
+    };
+    
     private final Handler mHandler = new Handler();
     
     @Override
@@ -31,11 +39,15 @@ public class LauncherActivity extends Activity {
         }
         
         // Show the splash screen then start the main activity
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(LauncherActivity.this, MainActivity.class));
-            }
-        }, SPLASH_TIMEOUT);
+        mHandler.postDelayed(mStartMainActivity, SPLASH_TIMEOUT);
+    }
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+        
+        // If the user has stopped the launcher activity, don't
+        // continue launching the main activity.
+        mHandler.removeCallbacks(mStartMainActivity);
     }
 }
