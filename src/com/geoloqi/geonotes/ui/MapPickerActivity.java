39050622ae9e4de02f1c6371d46fbcd6ab4dd4be
Tristan Waddington <tristan.waddington@gmail.com>
2012-07-09 15:07:27 -0700

    Remove unused imports.

--- src/com/geoloqi/geonotes/ui/MapPickerActivity.java
+++ src/com/geoloqi/geonotes/ui/MapPickerActivity.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 
 import com.actionbarsherlock.app.SherlockMapActivity;
 import com.actionbarsherlock.view.Menu;
