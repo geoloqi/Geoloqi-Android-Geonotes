09bffd82bbc5d2819a05735127ed1cdab5e6be5e
Tristan Waddington <tristan.waddington@gmail.com>
2012-07-09 18:20:13 -0700

    Display the created_at date for the geonotes in the list.

--- src/com/geoloqi/geonotes/widget/GeonoteListAdapter.java
+++ src/com/geoloqi/geonotes/widget/GeonoteListAdapter.java
@@ -1,5 +1,10 @@
 package com.geoloqi.geonotes.widget;
 
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.json.JSONObject;
 
 import android.content.Context;
@@ -59,8 +64,15 @@ public class GeonoteListAdapter extends ArrayAdapter<JSONObject> {
         JSONObject geonote = getItem(position);
         holder.text1.setText(geonote.optString("text"));
         
-        // TODO: 6 minutes ago | Rose Test Garden
-        holder.text2.setText(geonote.optString("place_name"));
+        // Format the metadata line
+        String subText = formatTimestamp(
+                geonote.optLong("date_created_ts") * 1000);
+        
+        String placeName = geonote.optString("place_name");
+        if (!TextUtils.isEmpty(placeName)) {
+            subText = String.format("%s | %s", subText, placeName);
+        }
+        holder.text2.setText(subText);
         
         // Hide the description TextView if it is empty so
         // the name field will be centered.
@@ -71,5 +83,26 @@ public class GeonoteListAdapter extends ArrayAdapter<JSONObject> {
         }
         
         return convertView;
+    }
+    
+    /**
+     * Format the created_at timestamp.
+     * 
+     * @param timestamp
+     * @return a formatted String.
+     */
+    private String formatTimestamp(long timestamp) {
+        Date date = new Date(timestamp);
+        
+        // Create our date formatter
+        SimpleDateFormat formatter = new SimpleDateFormat("MMM d, h:mma",
+                Locale.US);
+        
+        // Override the default AM/PM String values
+        DateFormatSymbols symbols = formatter.getDateFormatSymbols();
+        symbols.setAmPmStrings(new String[] { "am", "pm" });
+        formatter.setDateFormatSymbols(symbols);
+        
+        return formatter.format(date);
     }
 }
